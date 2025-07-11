76a97ab5e93a594a16057e4c85ab65eee265243b
Vladimir Khlebnikov <k2spam2@gmail.com>
2025-07-11 23:51:49 +0300

    fix

--- simple-training-bias.py
+++ simple-training-bias.py
@@ -1,6 +1,6 @@
 data = [(1, 3), (2, 5), (3, 7)] # [(x, y_true), ...] rule: y = 2*x + 1
 
-w, b = 0.5, 0.1 # weight and bias
+w, b = 0.5, 0.1 # start weight and bias
 lr = 0.01 # learning rate
 
 def neuron(x, w, b):
@@ -8,13 +8,13 @@ def neuron(x, w, b):
 
 for epoch in range(1000):
   for (x, y_true) in data:
-    y_pred = neuron(x, y_true, b) # prediction
+    y_pred = neuron(x, w, b) # prediction
     
     grad_w = 2 * (y_pred - y_true) * x
     grad_b = 2 * (y_pred - y_true)
 
     w -= lr * grad_w
-    b -= lr * grad_w
+    b -= lr * grad_b
 
 input = 7
 output = 7 * 2 + 1
